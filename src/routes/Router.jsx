0025a7980739706a61f0246e703223a8220ba1d3
Arifprogrammer <arifmgtcu@gmail.com>
2023-06-08 00:19:51 +0600

    added 404/erro page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../layouts/Home";
 import HomePage from "../pages/HomePage/HomePage/HomePage";
+import ErrorPage from "../pages/shared/Navbar/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
